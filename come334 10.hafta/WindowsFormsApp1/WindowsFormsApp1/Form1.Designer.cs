5ca61b442a68ba8fd8697004f86ad4461296f2ac
Taha Mert Cebecik <142932048+TahaMertCebecik@users.noreply.github.com>
2024-05-08 15:22:21 +0300

    10 hafta

---
+++ come334 10.hafta/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Depth:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Length Scale:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "D Theta";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(113, 42);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(113, 78);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(113, 153);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(203, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 370);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ come334 10.hafta/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawBranch(Graphics gr , Pen pen,int depth, float x, float y,  float length, float theta,float lengt_scale,float dtheta)
+        {
+            float x1 = (float)(x + length*Math.Cos(theta));
+            float y1 =(float)(y + length*Math.Sin(theta));
+            gr.DrawLine(pen, x, y, x1, y1);
+            // ıf depth>1 , draw the attached branches.
+            if(depth > 1 ) {
+            
+            DrawBranch(gr,pen, depth - 1, x1,y1,length* lengt_scale, theta+dtheta,lengt_scale,dtheta);
+            DrawBranch(gr, pen, depth - 1, x1, y1, length * lengt_scale, theta - dtheta, lengt_scale, dtheta);
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int depth = (int)numericUpDown1.Value;
+            if (!float.TryParse(textBox1.Text, out float lengt_scale))
+            {
+                // Display an error message if parsing fails
+                MessageBox.Show("Invalid length scale value!");
+                return;
+            }
+
+
+
+
+            // Call the DrawBranch method with the specified depth
+            using (Graphics gr = pictureBox1.CreateGraphics())
+            {
+                // Define parameters for the DrawBranch method
+                float x = pictureBox1.Width / 2; // X-coordinate of the starting point
+                float y = pictureBox1.Height;     // Y-coordinate of the starting point
+                float length = 100;                // Length of the main branch
+                float theta = -(float)Math.PI / 2; // Initial angle (straight up)
+                      // Length scale factor for child branches
+                float dTheta = (float)Math.PI / 4; // Angle change for child branches
+
+                // Create a Pen for drawing
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    // Clear the PictureBox before drawing
+                    gr.Clear(Color.White);
+
+                    // Call the DrawBranch method with the specified parameters
+                    DrawBranch(gr, pen, depth, x, y, length, theta, lengt_scale, dTheta);
+                }
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            // Get the new length value from the NumericUpDown control
+            float length = (float)numericUpDown2.Value;
+
+            // Call the DrawBranch method with the updated length value and other parameters
+            using (Graphics gr = pictureBox1.CreateGraphics())
+            {
+                // Get the depth value from the NumericUpDown control
+                int depth = (int)numericUpDown1.Value;
+
+                // Get the length scale value from the TextBox control
+                if (!float.TryParse(textBox1.Text, out float lengthScale))
+                {
+                    // Display an error message if parsing fails
+                    MessageBox.Show("Invalid length scale value!");
+                    return;
+                }
+
+                // Define other parameters for the DrawBranch method
+                float x = pictureBox1.Width / 2; // X-coordinate of the starting point
+                float y = pictureBox1.Height;     // Y-coordinate of the starting point
+                float theta = -(float)Math.PI / 2; // Initial angle (straight up)
+                float dTheta = (float)Math.PI / 4; // Angle change for child branches
+
+                // Create a Pen for drawing
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    // Clear the PictureBox before drawing
+                    gr.Clear(Color.White);
+
+                    // Call the DrawBranch method with the specified parameters
+                    DrawBranch(gr, pen, depth, x, y, length, theta, lengthScale, dTheta);
+                }
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            float dTheta = (float)numericUpDown3.Value;
+
+            // Call the DrawBranch method with the updated dTheta value and other parameters
+            using (Graphics gr = pictureBox1.CreateGraphics())
+            {
+                // Get the depth value from the NumericUpDown control
+                int depth = (int)numericUpDown1.Value;
+
+                // Get the length scale value from the TextBox control
+                if (!float.TryParse(textBox1.Text, out float lengthScale))
+                {
+                    // Display an error message if parsing fails
+                    MessageBox.Show("Invalid length scale value!");
+                    return;
+                }
+
+                // Get the length value from the NumericUpDown control
+                float length = (float)numericUpDown2.Value;
+
+                // Define other parameters for the DrawBranch method
+                float x = pictureBox1.Width / 2; // X-coordinate of the starting point
+                float y = pictureBox1.Height;    // Y-coordinate of the starting point
+                float theta = -(float)Math.PI / 2; // Initial angle (straight up)
+
+                // Create a Pen for drawing
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    // Clear the PictureBox before drawing
+                    gr.Clear(Color.White);
+
+                    // Call the DrawBranch method with the specified parameters
+                    DrawBranch(gr, pen, depth, x, y, length, theta, lengthScale, dTheta);
+                }
+            }
+        }
+    }
+}
