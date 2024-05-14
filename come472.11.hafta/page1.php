7f75d9b0aff910025968df0da554bb18a435090c
Taha Mert Cebecik <142932048+TahaMertCebecik@users.noreply.github.com>
2024-05-14 15:06:20 +0300

    come 472 11 hafta
    11.hafta

---
+++ come472.11.hafta/page1.php
@@ -0,0 +1,18 @@
+<?php
+
+echo '
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <a href="localhost:8081" /a>
+</head>
+<body>
+    <h1>html inside the php</h1>
+</body>
+</html>
+';
+?>
