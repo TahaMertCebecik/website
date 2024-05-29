846c652064a7f21f7353d0849d146bd0f494c4f6
Taha Mert Cebecik <142932048+TahaMertCebecik@users.noreply.github.com>
2024-05-29 13:52:43 +0300

    reader

---
+++ week10-1/week10-1/week08/week08/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace week08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContent.Location = new System.Drawing.Point(12, 48);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(459, 330);
+            this.txtContent.TabIndex = 0;
+            this.txtContent.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(110, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "&Open Text File...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(129, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "&Save As...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 390);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtContent);
+            this.Name = "Form1";
+            this.Text = "MZG Text File Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtContent;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ week10-1/week10-1/week08/week08/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text file|*.txt;*.csv";
+            var dialogResult = dialog.ShowDialog();
+            if(dialogResult == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(dialog.FileName))
+                    {
+                        var content = sr.ReadToEnd();
+                        txtContent.Text = content;
+                    }
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("IOExcelption: "+ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("UnknownExcelption: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text file|*.txt";
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(dialog.FileName))
+                    {
+                        writer.Write(txtContent.Text);
+                        MessageBox.Show("Saved. " + dialog.FileName);
+                    }
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("IOExcelption: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("UnknownExcelption: " + ex.Message);
+                }
+           
+            
+        }
+    }
+}
